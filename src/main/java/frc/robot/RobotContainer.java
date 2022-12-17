955325683aa667fc13bb973efc6cfd4f5e338505
avalanche-svg <edwardhfoster01@gmail.com>
2022-12-17 10:24:19 -0500

    Messed around with auton to attempt to get it working.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -87,7 +87,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
         // 1. Create trajectory settings
         TrajectoryConfig trajectoryConfig = new TrajectoryConfig(
-                DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+                DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND_TRAJECTORY,
                 DrivetrainSubsystem.MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
                         .setKinematics(Constants.kDriveKinematics);
 
@@ -97,7 +97,8 @@ public class RobotContainer {
                 List.of(
                         new Translation2d(1, 0),
                         new Translation2d(1, -1)),
-                new Pose2d(2, -1, Rotation2d.fromDegrees(180)),
+                new Pose2d(2, -1, Rotation2d.fromDegrees(-45)),
+
                 trajectoryConfig);
 
         // 3. Define PID controllers for tracking trajectory
