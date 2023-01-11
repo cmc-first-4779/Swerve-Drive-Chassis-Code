c0ef9ffc10ce2517b52eade4b28b7239d17a7e27
avalanche-svg <edwardhfoster01@gmail.com>
2023-01-10 20:05:46 -0500

    Added an end boolean to balance test

--- src/main/java/frc/robot/commands/balanceTest.java
+++ src/main/java/frc/robot/commands/balanceTest.java
@@ -39,7 +39,9 @@ public class balanceTest extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_drive.stopModules();
+  }
 
   // Returns true when the command should end.
   @Override
