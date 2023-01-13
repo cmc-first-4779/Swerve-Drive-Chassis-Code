912a7f4ac75b3f070c1b5028bbcb32207f783977
avalanche-svg <edwardhfoster01@gmail.com>
2023-01-12 19:05:01 -0500

    added an output to the smart dashboard for the Pitch

--- src/main/java/frc/robot/commands/balanceTest.java
+++ src/main/java/frc/robot/commands/balanceTest.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -28,6 +29,7 @@ public class balanceTest extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    SmartDashboard.putNumber("Robot Pitch", m_pigeonPitch);
     if (m_pigeonPitch >= Constants.PIGEON_DEADBAND) {
        m_drive.driveStraightSlow();
     }
